5b087685e1193a449f24601aeda2b087df024106
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-23 22:39:27 -0300

    feat swagger: Adicionar geração e exibição de Documentação da API
    commit 12c5946fb3dc921abd7898d6953afa44dd80be21
    Author: Rodolpho Alves <rodolpho.castro.a@gmail.com>
    Date:   Sat May 23 22:30:49 2020 -0300
    
        feat swagger: Configurar o Module para gerar os swagger.json e apresentar o swagger ui
    
    commit 0b6e3adb9be15e0a6b04ea0f8c34e358272b344a
    Author: Rodolpho Alves <rodolpho.castro.a@gmail.com>
    Date:   Sat May 23 22:22:32 2020 -0300
    
        chore: adicionar dependência ao Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const openApiOptions = new DocumentBuilder()
+    .setTitle('Teste Backend')
+    .setDescription('API para o teste de backend da Skore.IO')
+    .setVersion('0.1')
+    .build();
+  const openApiDocs = SwaggerModule.createDocument(app, openApiOptions);
+  SwaggerModule.setup('api', app, openApiDocs);
+
   await app.listen(3000);
 }
+
 bootstrap();
