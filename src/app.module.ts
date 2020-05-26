c22dd1961d5b1f18ececefbd16d1cd8a637bc97f
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-05-25 23:04:05 -0300

    refactor: Remover elementos não utilizados
    Remover o Controller e Services padrões do NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConteudosModule } from './conteudos/conteudos.module';
 
 @Module({
-  imports: [ConteudosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConteudosModule]
 })
-export class AppModule {}
+export class AppModule { }
